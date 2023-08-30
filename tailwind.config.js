271012d3b6d051530ce5b0954a9520bcc047c6ac
Ghazal Basalighe <ghazalbasaligheh@gmail.com>
2023-08-30 13:27:45 +0330

    [feat] font and primary color and rtl added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        iransans: ["IRANSans", "sans-serif"],
+      },
+      colors: {
+        primary: "#be3737",
+      },
+    },
   },
   plugins: [],
 };
